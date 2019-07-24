0310254c379a8be619c9f8f3ee52c5ed2f9c6d2f
Bharatramodedara <53074885+Bharatramodedara@users.noreply.github.com>
2019-07-24 18:23:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,85 @@
+
+var numofButtons=document.querySelectorAll(".drum").length;
+for(var i = 0; i<numofButtons;i++)
+{
+  document.querySelectorAll(".drum")[i].addEventListener("click",function handelclick(){
+  var buttonpressed = this.innerHTML;
+  buttonanimation(buttonpressed);
+  switch(buttonpressed){
+    case "w":
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+        break;
+    case "a":
+        var audio = new Audio("sounds/tom-2.mp3");
+        audio.play();
+        break;
+    case "s":
+        var audio = new Audio("sounds/tom-3.mp3");
+        audio.play();
+        break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+     break;
+    default: break;
+    }
+  });
+}
+
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+    buttonanimation(event.key);
+});
+function makeSound(buttonpressed){
+switch(buttonpressed){
+  case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+  case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+  case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+  case "d":
+    var audio = new Audio("sounds/tom-4.mp3");
+    audio.play();
+    break;
+  case "j":
+    var audio = new Audio("sounds/snare.mp3");
+    audio.play();
+    break;
+  case "k":
+    var audio = new Audio("sounds/crash.mp3");
+    audio.play();
+    break;
+  case "l":
+    var audio = new Audio("sounds/kick-bass.mp3");
+    audio.play();
+   break;
+  default: break;
+  }
+}
+
+function buttonanimation(currentkey){
+  var activeButton = document.querySelector("."+currentkey);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+  activeButton.classList.remove("pressed");}, 100);
+}
